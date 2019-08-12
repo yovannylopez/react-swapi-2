bc748c6123309617e3e3fcf3f6ac40e0c8a50e1f
Yovanny López <ylopezc@misena.edu.co>
2019-08-12 11:05:32 -0500

    funcionamiento general de componentes y assets

---
+++ src/components/DetailDescription.js
@@ -0,0 +1,19 @@
+// Dependencies
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// Styles
+import { Wrapper, Label, Text } from '../shared/styles/Detail';
+
+const DetailDescription = ({ description }) => (
+	<Wrapper>
+		<Label>Description</Label>
+		<Text>{description}</Text>
+	</Wrapper>
+);
+
+DetailDescription.propTypes = {
+	description: PropTypes.string
+};
+
+export default DetailDescription;
